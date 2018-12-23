c3f7618aef8ab36c8a34d5f5744e8293506fd28e
zeky <zotumlu78@gmail.com>
2018-12-23 16:03:14 -0500

    Core Java Finished

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -6,20 +6,27 @@ import java.util.Map;
 
 public class DetermineLargestWord {
     public static void main(String[] args) {
+
         /*
          Implement to Find the length and longest word in the given sentence below.
          Should return "10 biological".
          */
         String s="Human brain is a biological learning machine";
+
+        System.out.println(findTheLargestWord(s));
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
+       }
 
-    }
 
     public static Map<Integer, String> findTheLargestWord(String wordGiven){
         Map<Integer, String> map = new HashMap<Integer, String>();
         String st = "";
         //implement
+        String s="Human brain is a biological learning machine";
+
+        System.out.println(findTheLargestWord(s));
+
 
         return map;
     }
--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,17 +1,40 @@
 package string.problems;
 
+import java.util.Map;
+
 /**
  * Created by mrahman on 04/22/17.
  */
 public class DuplicateWord {
 
     public static void main(String[] args) {
+
         /*
          * Write a java program to find the duplicate words and their number of occurrences in the string.
          * Also Find the average length of the words.
          */
 
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+
+        System.out.println(DuplicateWord(st));
+
+    }
+
+    public static String DuplicateWord(String str){
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        String [] arr =str.split("");
+
+        for (String x: arr);
+        if(map.get(x) !=null) {
+            map.put(x, map.get(x) + 1);
+        }
+        else {
+            map.put(x, 1);
+        }
+        for (Map.Entry entry: map.entrySet()){
+            resultHolder +=entry.getKey() +": "+entry.getValue()+",";
+        }
+        return resultHolder;
 
     }
 
